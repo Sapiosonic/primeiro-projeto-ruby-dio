d1af9bcea4bddf897c8489523eb23013c6f9664c
Radamés <radames.idiomas@gmail.com>
2024-02-08 23:27:07 -0300

    desafio Ruby

---
+++ biblioteca.rb
@@ -0,0 +1,7 @@
+print "Informe seu nome: "
+nome = gets.chomp
+print "Informe seu sobrenome: "
+sobrenome = gets.chomp
+print "Informe sua idade: "
+idade = gets.chomp.to_i
+puts "Seu nome é #{nome} #{sobrenome} e sua idade é #{idade} anos!"
